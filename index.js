f389c1b6b16f760bc09bfe0f6121fd1c11143485
1 <kutsarb@gmail.com>
2020-10-27 18:04:02 -0400

    updated index file

--- index.js
+++ index.js
@@ -43,24 +43,18 @@ const questions = [
 ];
 
 // function to write README file
-
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+};
 
 // function to initialize program
 function init() {
     inquirer.prompt(questions) // .then function
         .then(function (data) {
-            const queryUrl = `https://api.github.com/users/${data.username}`;
-
-            axios.get(queryUrl).then(function (res) {
-                writeToFile()
+                writeToFile(data)
 
             });
-        });
     // call the function writeToFile using the data that we pass (ie the inquirer responses)
-};
-
-function writeToFile(fileName, data) {
-    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
 };
 // function call to initialize program
 init();
